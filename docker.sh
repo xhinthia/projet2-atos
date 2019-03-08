ed249984ce9c0fb7fd2e559296acc271d11428ef
root <root@projet1.us-east1-b.c.manifest-glyph-233615.internal>
2019-03-08 22:16:00 +0000

    correction script

--- docker.sh
+++ docker.sh
@@ -15,13 +15,12 @@ sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install docker-ce docker-ce-cli containerd.io
-sudo systemctl start docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo usermod -aG docker `whoami`
 sudo systemctl start docker
-docker-compose.yml up -d
+docker-compose up -d
 sudo docker exec -it wordpress bash
 echo "deb http://deb.debian.org/debian stretch-backports main" >> /etc/apt/sources.list
 apt update
